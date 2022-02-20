1dab51369c0f40fff8b01e3cf04db10aae7a7cdd
Tushar Pardhe <tushar.pardhe@airtel.com>
2022-02-20 18:29:21 +0530

    removed hot module

--- build-utils/webpack.dev.js
+++ build-utils/webpack.dev.js
@@ -3,16 +3,15 @@ const webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  mode: 'development',
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new Dotenv({
-      path: path.resolve(__dirname, '..', './.env.development'),
-    }),
-  ],
-  devServer: {
-    contentBase: path.resolve(__dirname, '..', './dist'),
-    hot: true,
-  },
-  devtool: 'eval-source-map',
+    mode: "development",
+    plugins: [
+        new Dotenv({
+            path: path.resolve(__dirname, "..", "./.env.development"),
+        }),
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, "..", "./dist"),
+        hot: true,
+    },
+    devtool: "eval-source-map",
 };
